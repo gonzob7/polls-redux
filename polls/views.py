15687d45cbe77f0f034acad38abcbc2b334d8fbd
Gonzalo Birrueta <birruetabusiness@gmail.com>
2020-06-15 21:19:52 -0700

    fixed index

--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,6 @@
 
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
-from django.template import loader
 from django.http import Http404
 
 from .models import Question
@@ -10,11 +9,8 @@ from .models import Question
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('polls/index.html')
-    context = {
-        'latest_question_list': latest_question_list,
-    }
-    return HttpResponse(template.render(context, request))
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
 
 '''views with question number you're currently on'''
 def detail(request, question_id):
