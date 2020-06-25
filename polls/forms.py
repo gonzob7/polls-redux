8e2daa1c65d096d17c35e3e4e2fc6d6bdb406c20
Gonzalo Birrueta <birruetabusiness@gmail.com>
2020-06-24 18:15:47 -0700

    new forms class for choice

--- polls/forms.py
+++ polls/forms.py
@@ -6,3 +6,8 @@ class QuestionCreateForm(forms.ModelForm):
     class Meta:
         model = Question
         fields = ['question_text', 'pub_date']
+
+class ChoiceCreateForm(forms.ModelForm):
+    class Meta:
+        model = Choice
+        fields = ['choice_text']
